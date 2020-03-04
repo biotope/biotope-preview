5ec7c7b5efbb83c2d50f0bb6c4e56c83f58ded96
kai.wissler@virtual-identity.com <kai.wissler@virtual-identity.com>
2020-03-04 08:01:52 +0100

    fix: resolve path correctly

--- src/file-handlers/create-stories-file-for-story-config.ts
+++ src/file-handlers/create-stories-file-for-story-config.ts
@@ -1,10 +1,11 @@
+import path = require('path');
 import * as fs from 'fs';
 import { IStoryConfiguration } from "../interfaces/i-story-configuration";
 import { generateStoryHtml } from '../html-builders/generate-story-html';
 
 export const createStoriesFileForStoryConfig = (storyConfig: IStoryConfiguration): Promise<void> => {
     return new Promise((resolve, reject) => {
-        fs.writeFile(`${__dirname}/stories/${storyConfig.htmlTagName}.stories.js`, generateStoryHtml(storyConfig), (err) => {
+        fs.writeFile(path.resolve(__dirname, `../../stories/${storyConfig.htmlTagName}.stories.js`), generateStoryHtml(storyConfig), (err) => {
             if (err) reject();
             console.log(generateStoryHtml(storyConfig));
             console.log('Saved!');
