0c0375eadbe28775ac59163eab67a8cf84844573
kai.wissler@virtual-identity.com <kai.wissler@virtual-identity.com>
2020-03-05 10:49:02 +0100

    feat: use recursive

--- src/run-creation-of-stories-files.ts
+++ src/run-creation-of-stories-files.ts
@@ -1,20 +1,15 @@
 import path = require('path');
-import { getContentsOfDirectory } from './file-handlers/get-contents-of-directory';
 import { filterFilePathsForPackageJson } from './file-handlers/filter-file-paths-for-package-json';
 import { createStoriesFileForStoryConfig } from './file-handlers/create-stories-file-for-story-config';
 import { getJsonContent } from './file-handlers/get-json-content';
 import { IStoryConfiguration } from './interfaces/i-story-configuration';
+import recursive from 'recursive-readdir';
 
 const projectBasePath = path.resolve(__dirname).split('/node_modules')[0];
 
 export const runCreationOfStoriesFiles = async (componentsSrcDir: string = 'src/components') => {
-    const subFolders = await getContentsOfDirectory(`${projectBasePath}/${componentsSrcDir}`, true, true);
-    const filesPaths = await Promise.all(
-        subFolders.map(
-            subFolder => getContentsOfDirectory(subFolder, true, false)
-        )
-    );
-    const filteredFilesPaths = filterFilePathsForPackageJson([].concat.apply([], filesPaths));
+    const recursiveFilePaths = await recursive(`${projectBasePath}/${componentsSrcDir}`);
+    const filteredFilesPaths = filterFilePathsForPackageJson([].concat.apply([], recursiveFilePaths));
     const storyConfigs = filteredFilesPaths.map((filePath: string) => getJsonContent(filePath));
     await Promise.all(storyConfigs.map((storyConfig: IStoryConfiguration) => createStoriesFileForStoryConfig(storyConfig)));
 }
