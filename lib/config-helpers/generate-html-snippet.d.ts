624571f94fc905bef05b16b35ba9350779855872
kai.wissler@virtual-identity.com <kai.wissler@virtual-identity.com>
2020-03-06 15:17:34 +0100

    feat: add templating helper

---
+++ lib/config-helpers/generate-html-snippet.d.ts
@@ -0,0 +1,3 @@
+import { IHtmlElementConfiguration } from "../interfaces/i-html-element-configuration";
+export declare const generateHtmlSnippet: (config: IHtmlElementConfiguration) => string;
+export declare const stripHtmlConfigFromKnobs: (config: IHtmlElementConfiguration) => IHtmlElementConfiguration;
