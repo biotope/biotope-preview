d2c248a5e7074f0cb4599549f900e698ad96741b
kai.wissler@virtual-identity.com <kai.wissler@virtual-identity.com>
2020-03-06 12:20:36 +0100

    fix: fix ts compilation

--- src/gulp.ts
+++ src/gulp.ts
@@ -1,21 +1,14 @@
 const { src, dest } = require('gulp');
 const ts = require('gulp-typescript');
-const rename = require('gulp-rename');
 const { build } = require('./build');
 const { serve } = require('./serve');
 const path = require('path');
 const projectBasePath = path.resolve(__dirname).split('/node_modules')[0];
 
 export function compileTsConfigs() {
-    var index = 0;
     return src(`${projectBasePath}/src/components/**/preview/index.ts`)
-        .pipe(ts({
-            outFile: "config.js"
-        }))
-        .pipe(rename((path: any) => {
-            path.basename += ("-" + index++);
-        }))
-        .pipe(dest('configs'));
+        .pipe(ts())
+        .pipe(dest(`${__dirname}/configurations`));
 }
 
 export function buildPreview() {
