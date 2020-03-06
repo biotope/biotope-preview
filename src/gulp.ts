f0dacd4773fb6f629cebfde1cad877d83fe76315
kai.wissler@virtual-identity.com <kai.wissler@virtual-identity.com>
2020-03-06 13:13:54 +0100

    feat: use imported configs

--- src/gulp.ts
+++ src/gulp.ts
@@ -6,7 +6,7 @@ const path = require('path');
 const projectBasePath = path.resolve(__dirname).split('/node_modules')[0];
 
 export function compileTsConfigs() {
-    return src(`${projectBasePath}/src/components/**/preview/index.ts`)
+    return src(`${projectBasePath}/src/components/**/preview/*.ts`)
         .pipe(ts())
         .pipe(dest(path.resolve(`${__dirname}/../configurations`)));
 }
--- src/run-creation-of-stories-files.ts
+++ src/run-creation-of-stories-files.ts
@@ -1,15 +1,10 @@
-import path = require('path');
-import { filterFilePathsForPackageJson } from './file-handlers/filter-file-paths-for-package-json';
 import { createStoriesFileForConfig } from './file-handlers/create-stories-file-for-config';
-import { getJsonContent } from './file-handlers/get-json-content';
-import { IComponentConfiguration } from './interfaces/i-component-configuration';
 const recursive = require("recursive-readdir");
 
-const projectBasePath = path.resolve(__dirname).split('/node_modules')[0];
-
 export const runCreationOfStoriesFiles = async (componentsSrcDir: string = 'src/components') => {
-    const recursiveFilePaths = await recursive(`${projectBasePath}/${componentsSrcDir}`);
-    const filteredFilesPaths = filterFilePathsForPackageJson([].concat.apply([], recursiveFilePaths));
-    const configs = filteredFilesPaths.map((filePath: string) => getJsonContent(filePath));
-    await Promise.all(configs.map((config: IComponentConfiguration) => createStoriesFileForConfig(config)));
+    const recursiveFilePaths = await recursive(`${__dirname}/../configurations`);
+    const importedConfigurations = recursiveFilePaths.map((filePath: string) => filePath.indexOf('index.ts') !== -1 ? require(filePath) : {});
+    //const filteredFilesPaths = filterFilePathsForPackageJson([].concat.apply([], recursiveFilePaths));
+    //const configs = recursiveFilePaths.map((filePath: string) => getJsonContent(filePath));
+    await Promise.all(importedConfigurations.map((config: any) => createStoriesFileForConfig(config.default)));
 }
