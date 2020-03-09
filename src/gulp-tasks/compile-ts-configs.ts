ab2609b5fc14ad4666eaaae9a2308ab81ca9d377
kai.wissler@virtual-identity.com <kai.wissler@virtual-identity.com>
2020-03-09 08:22:46 +0100

    feat: add gulp debug

--- src/gulp-tasks/compile-ts-configs.ts
+++ src/gulp-tasks/compile-ts-configs.ts
@@ -1,11 +1,13 @@
 const { src, dest } = require('gulp');
 const ts = require('gulp-typescript');
+const debug = require('gulp-debug');
 const path = require('path');
 const projectBasePath = path.resolve(__dirname).split('/node_modules')[0];
 
 export function compileTsConfigs() {
     console.log("Compiling preview configurations...");
     return src(`${projectBasePath}/src/components/**/preview/*.ts`)
+        .pipe(debug())
         .pipe(ts())
         .pipe(dest(path.resolve(`${__dirname}/../../configurations`)));
 }
