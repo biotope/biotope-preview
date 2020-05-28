bae1f5688dbe88a81a6da0b39917cc0fd5e23652
kai.wissler@virtual-identity.com <kai.wissler@virtual-identity.com>
2020-05-28 16:46:55 +0200

    feat: use another path

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,6 @@ module.exports = {
         '@storybook/addon-knobs',
         '@storybook/addon-storysource',
         '@storybook/addon-viewport',
-        'node_modules/@biotope/preview/.storybook/source-code-utils/storybook-register.js',
+        '../../@biotope/preview/.storybook/source-code-utils/storybook-register.js',
     ]
 };
