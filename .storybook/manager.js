d47ae83d86fa54453ba5c768b87aad42326ac2f5
Benjamin Konopka <benjamin.konopka@icloud.com>
2020-05-29 11:37:16 +0200

    feat: 🎸 adapt path to theme

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,6 +1,11 @@
 import { addons } from '@storybook/addons';
 import biotopeTheme from './biotope-theme';
 
+const path = require('path');
+
+const projectBasePath = path.resolve(__dirname).split('/node_modules')[0];
+const previewConfig = require(`${projectBasePath}/preview-config.js`);
+
 addons.setConfig({
-  theme: biotopeTheme,
+  theme: previewConfig.theme || biotopeTheme,
 });
