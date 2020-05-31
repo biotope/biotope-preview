83fd21992f6f55885495aee9e66f4d23e46b6b1a
Benjamin Konopka <benjamin.konopka@icloud.com>
2020-05-31 15:55:32 +0200

    fix: 🐛 debuging adaption

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -3,6 +3,9 @@ import biotopeTheme from './biotope-theme';
 
 const path = require('path');
 
+console.log("base path" + projectBasePath);
+console.log("preview config" + previewConfig);
+
 const projectBasePath = path.resolve(__dirname).split('/node_modules')[0];
 const previewConfig = require(`${projectBasePath}/preview-config.js`);
 
