a51070475bba1adf93c017887024cd0541d55570
kai.wissler@virtual-identity.com <kai.wissler@virtual-identity.com>
2020-06-02 12:43:08 +0200

    feat: add docs addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,7 @@ module.exports = {
         '@storybook/addon-a11y',
         '@storybook/addon-knobs',
         '@storybook/addon-viewport',
+        '@storybook/addon-docs/html/preset',
         '@biotope/storybook-addon-compiled-code',
     ]
 };
