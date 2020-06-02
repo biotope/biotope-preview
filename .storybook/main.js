08571b1d45d9216ad894e74fa91b29f848335956
kai.wissler@virtual-identity.com <kai.wissler@virtual-identity.com>
2020-06-02 13:07:00 +0200

    feat: fix addon usage in storybook main js

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,7 +4,7 @@ module.exports = {
         '@storybook/addon-a11y',
         '@storybook/addon-knobs',
         '@storybook/addon-viewport',
-        '@storybook/addon-docs/html/preset',
+        '@storybook/addon-docs',
         '@biotope/storybook-addon-compiled-code',
     ]
 };
