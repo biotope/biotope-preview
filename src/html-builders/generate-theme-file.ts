8ae434c419e533fd36572a8e557dc724c3bc173d
Benjamin Konopka <benjamin.konopka@icloud.com>
2020-06-02 13:55:16 +0200

    fix: 🐛 try something else

--- src/html-builders/generate-theme-file.ts
+++ src/html-builders/generate-theme-file.ts
@@ -1,6 +1,7 @@
 // import {writeFileSync} from "fs";
 const fs = require('fs');
+const path = require('path');
 
 export const generateThemeFile = (themefile: any) => {
-  fs.writeFileSync("../../.storybook/theme.js", themefile.theme);
+  fs.writeFileSync(`${path.resolve(__dirname, '../.storybook')}/theme.js`, themefile.theme);
 }
