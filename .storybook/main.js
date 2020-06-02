02dff78c8993cdc032185d4f307ccb6cbe013112
kai.wissler@virtual-identity.com <kai.wissler@virtual-identity.com>
2020-06-02 11:22:23 +0200

    feat: remove docs addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,9 +2,8 @@ module.exports = {
     stories: ['../stories/*.stories.[tj]s'],
     addons: [
         '@storybook/addon-a11y',
-        '@storybook/addon-docs',
         '@storybook/addon-knobs',
-        '@storybook/addon-compiled-code',
-        '@storybook/addon-viewport'
+        '@storybook/addon-viewport',
+        '@biotope/storybook-addon-compiled-code',
     ]
 };
