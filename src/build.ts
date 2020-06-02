4a089d9cf61bac8b7e0f12e47249423ef438601d
Rebecca Emmanuel <rebecca.emmanuel@virtual-identity.com>
2020-06-02 14:03:30 +0200

    refactor: 💡 try and catch for async functions

--- src/build.ts
+++ src/build.ts
@@ -1,6 +1,10 @@
 export const build = async () => {
-    const { compileTsConfigs } = require('./gulp-tasks/compile-ts-configs');
-    const { buildPreview } = require('./gulp-tasks/build-preview');
+  const { compileTsConfigs } = require("./gulp-tasks/compile-ts-configs");
+  const { buildPreview } = require("./gulp-tasks/build-preview");
+  try {
     await compileTsConfigs();
     await buildPreview();
-}
+  } catch (err) {
+    console.log("Ooops...something went wrong!");
+  }
+};
--- src/serve.ts
+++ src/serve.ts
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
 (async () => {
-    const { compileTsConfigs } = require('./gulp-tasks/compile-ts-configs');
-    const { servePreview } = require('./gulp-tasks/serve-preview');
+  const { compileTsConfigs } = require("./gulp-tasks/compile-ts-configs");
+  const { servePreview } = require("./gulp-tasks/serve-preview");
+  try {
     await compileTsConfigs();
     await servePreview();
+  } catch (err) {
+    console.log("Ooops...something went wrong!");
+  }
 })();
