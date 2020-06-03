959a3d5f3da4134c94bf3f30b021485dd8b505e9
kai.wissler@virtual-identity.com <kai.wissler@virtual-identity.com>
2020-06-03 12:51:32 +0200

    fix: use process.cwd() instead of custom path

--- src/gulp-tasks/get-global-config.ts
+++ src/gulp-tasks/get-global-config.ts
@@ -3,12 +3,10 @@ import { FALLBACK_THEME } from '../constants/fallback-theme';
 import { IGlobalConfigurationInput } from '../interfaces/i-global-configuration-input';
 
 require('typescript-require');
-const path = require('path');
-const projectBasePath = path.resolve(__dirname).split('/node_modules')[0];
 
 export function getGlobalConfig(): IGlobalConfiguration {
     console.log("Reading global config...");
-    const previewConfig: IGlobalConfigurationInput = require(`${projectBasePath}/preview-config.ts`).previewConfig;
+    const previewConfig: IGlobalConfigurationInput = require(`${process.cwd()}/preview-config.ts`).previewConfig;
     return {
         globalResources: previewConfig.globalResources || [],
         componentsSrcDir: previewConfig.componentsSrcDir || "src/components",
