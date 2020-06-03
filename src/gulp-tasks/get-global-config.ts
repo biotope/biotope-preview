abd9dd3484362b2402c64709e1dffb15c0a256a0
kai.wissler@virtual-identity.com <kai.wissler@virtual-identity.com>
2020-06-03 11:43:22 +0200

    feat: require with typescript require

--- src/gulp-tasks/get-global-config.ts
+++ src/gulp-tasks/get-global-config.ts
@@ -2,11 +2,12 @@ import { IGlobalConfiguration } from "../interfaces/i-global-configuration";
 import { FALLBACK_THEME } from '../constants/fallback-theme';
 
 const path = require('path');
+const typescriptRequire = require('typescript-require');
 const projectBasePath = path.resolve(__dirname).split('/node_modules')[0];
 
 export function getGlobalConfig(): IGlobalConfiguration {
     console.log("Reading global config...");
-    const previewConfig: IGlobalConfiguration = require(`${projectBasePath}/preview-config.ts`).previewConfig;
+    const previewConfig: IGlobalConfiguration = typescriptRequire(`${projectBasePath}/preview-config.ts`).previewConfig;
     return {
         globalResources: previewConfig.globalResources || [],
         componentsSrcDir: previewConfig.componentsSrcDir || "src/components",
