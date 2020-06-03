7a35d222f3b49713edd293657ca7b41356ad82e5
Benjamin Konopka <benjamin.konopka@icloud.com>
2020-06-03 10:04:12 +0200

    feat: 🎸 another test

--- src/html-builders/generate-theme-file.ts
+++ src/html-builders/generate-theme-file.ts
@@ -3,5 +3,11 @@ const fs = require('fs');
 const path = require('path');
 
 export const generateThemeFile = (themefile: any) => {
-  fs.writeFileSync(`${path.resolve(__dirname, '../../.storybook')}/theme.js`, themefile.theme.join(','), 'utf-8');
+  fs.writeFileSync(`${path.resolve(__dirname, '../../.storybook')}/theme.js`, `
+  import { create } from '@storybook/theming/create';
+
+  export default create(${
+    JSON.stringify(themefile.theme)
+  });
+  `);
 }
