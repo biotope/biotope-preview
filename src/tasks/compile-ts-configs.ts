73ed68400e60735b64a52cae9f40675d11993833
Rebecca Emmanuel <rebecca.emmanuel@virtual-identity.com>
2020-06-03 15:22:45 +0200

    refactor: 💡 remove gulp

--- src/tasks/compile-ts-configs.ts
+++ src/tasks/compile-ts-configs.ts
@@ -1,29 +1,41 @@
-// const ts = require("typescript");
-// const fs = require("fs");
+const ts = require("typescript");
+const fs = require("fs");
 const path = require("path");
 const recursive = require("recursive-readdir");
 const projectBasePath = path.resolve(__dirname).split("/node_modules")[0];
+const regex = new RegExp(".*/preview/index.ts$");
 import { getGlobalConfig } from "./get-global-config";
-// const transpileFile = (data: string) => {
-//   return ts.transpileModule(data, {}).outputText;
-// };
+const transpileFile = (data: string) => {
+  return ts.transpileModule(data, {}).outputText;
+};
 export async function compileTsConfigs() {
   console.log("Compiling preview configurations...");
   const globalConfig = getGlobalConfig();
-  const tsFilesPaths = await recursive( `${projectBasePath}/${globalConfig.componentsSrcDir}/../preview/index.ts`);
-  console.log(tsFilesPaths);
-//   fs.readfile(
-//     (err: string, data: string) => {
-//       if (err) {
-//         throw err;
-//       }
-//       const transpiledFile = transpileFile(data);
-//       fs.writeFile(path.resolve(`${__dirname}/../../configurations`), transpiledFile);
-//     }
-//   );
-
-  // return src(`${projectBasePath}/${globalConfig.componentsSrcDir}/**/preview/*.ts`)
-  //     .pipe(debug())
-  //     .pipe(ts())
-  //     .pipe(dest(path.resolve(`${__dirname}/../../configurations`)));
+  const allComponentsFiles = await recursive(
+    `${projectBasePath}/${globalConfig.componentsSrcDir}/`
+  );
+  const tsFilesPaths = allComponentsFiles.filter((path: string) =>
+    regex.test(path)
+  );
+  const transpiledFiles = tsFilesPaths.map((path: string) => {
+    fs.readFile((err: string, data: string) => {
+      if (err) {
+        throw err;
+      }
+      return transpileFile(data);
+    });
+  });
+  return new Promise((resolve, reject) => {
+    transpiledFiles.map((transpiledFile: string) => {
+      console.log(transpileFile);
+      fs.writeFile(
+        path.resolve(`${__dirname}/../../configurations/index.js`),
+        transpiledFile,
+        (err: string) => {
+          if (err) reject();
+        }
+      );
+    });
+    resolve();
+  });
 }
