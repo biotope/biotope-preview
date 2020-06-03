88f4f3bea7d7e5f4521b83933ba26b111fddeef6
Benjamin Konopka <benjamin.konopka@icloud.com>
2020-06-03 10:17:42 +0200

    test: 💍 and another one

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,14 +1,6 @@
 import { addons } from '@storybook/addons';
-import biotopeTheme from './biotope-theme';
-
-const path = require('path');
-
-
-const projectBasePath = path.resolve(__dirname).split('/node_modules')[0];
-const previewConfig = require(`${projectBasePath}/preview-config.js`);
-console.log("base path" + projectBasePath);
-console.log("preview config" + previewConfig);
+import { theme } from './theme';
 
 addons.setConfig({
-  theme: previewConfig.theme || biotopeTheme,
+  theme
 });
--- src/html-builders/generate-theme-file.ts
+++ src/html-builders/generate-theme-file.ts
@@ -2,12 +2,23 @@
 const fs = require('fs');
 const path = require('path');
 
+const fallbackTheme = {
+  base: 'light',
+
+  colorPrimary: '#607DBE',
+  colorSecondary: '#F07D61',
+
+  brandTitle: '@biotope/preview',
+  brandUrl: 'https://biotope.sh/',
+  brandImage: 'https://biotope.sh/_assets/biotope-logo.svg',
+}
+
 export const generateThemeFile = (themefile: any) => {
   fs.writeFileSync(`${path.resolve(__dirname, '../../.storybook')}/theme.js`, `
   import { create } from '@storybook/theming/create';
 
   export default create(${
-    JSON.stringify(themefile.theme)
+    JSON.stringify({...fallbackTheme, ...themefile.theme})
   });
   `);
 }
