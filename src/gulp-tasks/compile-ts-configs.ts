1da7b1be981e194ace92ef04c569a99c1960b113
kai.wissler@virtual-identity.com <kai.wissler@virtual-identity.com>
2020-06-03 13:30:43 +0200

    refactor: use process.cwd instead of custom path

--- src/gulp-tasks/compile-ts-configs.ts
+++ src/gulp-tasks/compile-ts-configs.ts
@@ -2,13 +2,12 @@ const { src, dest } = require('gulp');
 const ts = require('gulp-typescript');
 const debug = require('gulp-debug');
 const path = require('path');
-const projectBasePath = path.resolve(__dirname).split('/node_modules')[0];
 import { getGlobalConfig } from "./get-global-config";
 
 export function compileTsConfigs() {
     console.log("Compiling preview configurations...");
     const globalConfig = getGlobalConfig();
-    return src(`${projectBasePath}/${globalConfig.componentsSrcDir}/**/preview/*.ts`)
+    return src(`${process.cwd()}/${globalConfig.componentsSrcDir}/**/preview/*.ts`)
         .pipe(debug())
         .pipe(ts())
         .pipe(dest(path.resolve(`${__dirname}/../../configurations`)));
