000192fe8d6cb50e31107d3f0142d81240453fc7
Rebecca Emmanuel <rebecca.emmanuel@virtual-identity.com>
2020-06-03 19:19:13 +0200

    refactor: 💡 remove gulp

--- src/tasks/compile-ts-configs.ts
+++ src/tasks/compile-ts-configs.ts
@@ -3,14 +3,14 @@ const fs = require("fs");
 const path = require("path");
 const recursive = require("recursive-readdir");
 const projectBasePath = path.resolve(__dirname).split("/node_modules")[0];
-const regex = new RegExp("(.*)/preview/index.ts$");
+const regex = new RegExp(/.*\/(?<component>.*)\/preview\/(?<filename>.*)\.ts$/);
 import { getGlobalConfig } from "./get-global-config";
 
-const transpile = (data: string) => {
-    return ts.transpileModule(data, {}).outputText;
-  };
-const transpileFiles = (path: string) => {
-  return transpile(fs.readFileSync(path));
+const transpile = (tsSourceCode: string) => {
+  return ts.transpileModule(tsSourceCode, {}).outputText;
+};
+const transpileFile = (path: string) => {
+  return transpile(fs.readFileSync(path, "utf8"));
 };
 
 export async function compileTsConfigs() {
@@ -22,16 +22,15 @@ export async function compileTsConfigs() {
   const tsFilesPaths = allComponentsFiles.filter((path: string) =>
     regex.test(path)
   );
-  const transpiledFiles = tsFilesPaths.map((path: string) => {
-    transpileFiles(path);
-  });
-console.log(tsFilesPaths.forEach((path: string) => console.log(path.match(regex))
-))
+  const transpiledFiles = tsFilesPaths.map(transpileFile);
+
   return new Promise((resolve, reject) => {
-    transpiledFiles.forEach((transpiledFile: string) => {
-      fs.writeFileSync(
-        path.resolve(`${__dirname}/../../configurations/index.js`),
-        transpiledFile,);
+    transpiledFiles.forEach((transpiledCode: string, index: number) => {
+      const { component, filename } = tsFilesPaths[index].match(regex).groups;
+      const foldername = `${__dirname}/../../configurations/${component}`;
+      if (!fs.readdirSync(foldername)) fs.mkdirSync(foldername);
+      console.log(`${__dirname}/../../configurations/${component}/index.js`);
+      fs.writeFileSync(`${foldername}/${filename}.js`, transpiledCode);
     });
     resolve();
   });
