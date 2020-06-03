f43f8cc3c89344ece44314811baeff1ba67f40a7
kai.wissler@virtual-identity.com <kai.wissler@virtual-identity.com>
2020-06-03 15:09:57 +0200

    fix: add building of theme file to serve process

--- src/gulp-tasks/build-preview.ts
+++ src/gulp-tasks/build-preview.ts
@@ -1,7 +1,7 @@
 import { runCreationOfStoriesFiles } from "../run-creation-of-stories-files";
 import { runStorybook } from "../run-storybook";
 import { getGlobalConfig } from "./get-global-config";
-import { createThemeFile } from "../file-handlers/create-theme-file"
+import { createThemeFile } from "../file-handlers/create-theme-file";
 
 export async function buildPreview() {
     console.log("Building the preview...");
