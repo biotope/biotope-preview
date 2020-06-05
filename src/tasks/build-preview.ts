ed030c50b52ea66b3266809122f44858007e2d52
Rebecca Emmanuel <rebecca.emmanuel@virtual-identity.com>
2020-06-05 11:40:06 +0200

    refactor: 💡 error handling

--- src/tasks/build-preview.ts
+++ src/tasks/build-preview.ts
@@ -6,7 +6,7 @@ import { createThemeFile } from "../file-handlers/create-theme-file";
 export async function buildPreview() {
     console.log("Building the preview...");
     const globalConfig = getGlobalConfig();
-    await runCreationOfStoriesFiles(globalConfig.globalResources);
-    await createThemeFile(globalConfig.theme);
-    await runStorybook({ mode: 'static', staticDir: globalConfig.resourcesDir, outputDir: globalConfig.outputDir });
+    await runCreationOfStoriesFiles(globalConfig.globalResources).catch((err) => err);
+    await createThemeFile(globalConfig.theme).catch((err) => err);
+    await runStorybook({ mode: 'static', staticDir: globalConfig.resourcesDir, outputDir: globalConfig.outputDir }).catch((err) => err);
 }
