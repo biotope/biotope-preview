8a63787579adaae6296015b331c44229003af4c4
Rebecca Emmanuel <rebecca.emmanuel@virtual-identity.com>
2020-06-10 08:52:04 +0200

    refactor: 💡 gitignore

--- .storybook/theme.js
+++
@@ -1,5 +0,0 @@
-
-      import { create } from '@storybook/theming/create';
-
-      export default create({"base":"light","colorPrimary":"#607DBE","colorSecondary":"#F07D61","brandTitle":"@biotope/preview","brandUrl":"https://biotope.sh/","brandImage":"https://biotope.sh/_assets/biotope-logo.svg"});
-      
