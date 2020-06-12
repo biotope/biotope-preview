e2508dcf12929905cdbb589aedecad6088dd7138
Kai <kai.wissler@virtual-identity.com>
2020-06-12 13:50:18 +0200

    feat: also allow mjs global resources

--- src/file-handlers/create-tag-string-for-resource.ts
+++ src/file-handlers/create-tag-string-for-resource.ts
@@ -4,7 +4,7 @@ export const createTagStringForResource = (
   const fileTypeSeparator = /[^\\]*\.(\w+)$/;
   const separated = resourcePath.match(fileTypeSeparator);
   const fileType = separated ? separated[1] : undefined;
-  if (fileType === 'js') {
+  if (fileType === 'js' || fileType === 'mjs') {
     return `<script src="${resourcePath}" type="module" async charset="utf-8"></script>`;
   }
   if (fileType === 'css') {
