53e0b61195bdc628fdfe3f683e9eca04b2e651c8
Kai <kai.wissler@virtual-identity.com>
2020-06-17 16:34:55 +0200

    fix: add renderer for color knob

--- src/story-builders/knob-renderers/render-string-knob.test.ts
+++ src/story-builders/knob-renderers/render-string-knob.test.ts
@@ -7,7 +7,3 @@ test('returns text knob', () => {
 test('returns text knob with groupId', () => {
   expect(renderStringKnob({ label: 'knobName', defaultValue: 'text', groupId: '123' })).toBe('"${text(\'knobName\', \'text\', \'123\')}"');
 });
-
-test('returns color knob', () => {
-  expect(renderStringKnob({ label: 'knobName', defaultValue: '#fff', groupId: '123' }, 'color')).toBe('"${color(\'knobName\', \'#fff\', \'123\')}"');
-});
